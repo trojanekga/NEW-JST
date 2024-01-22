f19005d01a1b986b73310da47bbfe722c7bf66a3
Garrett Trojanek <gtrojan@wgu.edu>
2024-01-21 18:26:10 -0600

    Adding job entity with encapsulation/constructor

---
+++ JST/app/src/main/java/com/example/jobshoptrackapp/Job.java
@@ -0,0 +1,41 @@
+package com.example.jobshoptrackapp;
+
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "job_table")
+public class Job {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String title;
+    private String description;
+    private int priority;
+
+    public Job(String title, String description, int priority) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+}
