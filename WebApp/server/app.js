ac47b9a004609024afd2ace41390c7cfdc88985f
Garrett Trojanek <gtrojan@wgu.edu>
2024-02-12 21:34:50 -0600

    Database functionality added, CRUD

--- WebApp/server/app.js
+++ WebApp/server/app.js
@@ -13,7 +13,13 @@ app.use(express.urlencoded({extended : false}));
 
 // Create
 app.post('/insert', (request, response) => {
+    const { name } = request.body;
+    const db = dbService.getDbServiceInstance();
+    const result = db.insertNewName(name);
 
+    result
+    .then(data => response.json({ data: data}))
+    .catch(err => console.log(err));
 });
 
 // Read
@@ -29,10 +35,31 @@ app.get('/getAll', (request, response) => {
 })
 
 // Update
+app.patch('/update', (request, response) => {
+    const { id, name } = request.body;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.updateNameById(id, name);
+
+    result
+    .then(data => response.json({success : data}))
+    .catch(err => console.log(err));
+
+});
 
 
 
 // Delete
+app.delete('/delete/:id', (request, response) => {
+    const { id } = request.params;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.deleteRowById(id);
+
+    result
+    .then(data => response.json({success : data}))
+    .catch(err => console.log(err));
+})
 
 
 app.listen(process.env.PORT, () => console.log('App is running'));
--- WebApp/server/dbService.js
+++ WebApp/server/dbService.js
@@ -25,7 +25,7 @@ class DbService {
     }
     async getAllData() {
         try{
-            const response = await new Promise((resolve, reject)=> {
+            const response = await new Promise((resolve, reject) => {
                 //const query = "SELECT * FROM jobs WHERE id = ?"
                 const query = "SELECT * FROM jobs";
                 connection.query(query, (err, results) => {
@@ -33,12 +33,73 @@ class DbService {
                     resolve(results);
                 })
             });
-            //console.log(response);
             return response;
         } catch (error) {
             console.log(error);
         }
     }
+
+    async insertNewName(name) {
+        try {
+            const dateAdded = new Date();
+            const insertId = await new Promise((resolve, reject)=> {
+                //const query = "SELECT * FROM jobs WHERE id = ?"
+                const query = "INSERT INTO jobs (name, date_added) VALUES (?,?);";
+                connection.query(query, [name, dateAdded], (err, result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result.insertId);
+                })
+            });
+            return {
+                id: insertId,
+                name: name,
+                dateAdded: dateAdded
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    async deleteRowById(id) {
+        try {
+            id = parseInt(id, 10);
+            const response = await new Promise((resolve, reject)=> {
+                //const query = "SELECT * FROM jobs WHERE id = ?"
+                const query = "DELETE FROM jobs WHERE id = ?";
+
+                connection.query(query, [id], (err, result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result.affectedRows);
+                })
+            });
+
+            return response === 1 ? true : false;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    async updateNameById(id, name){
+        try {
+            id = parseInt(id, 10);
+            const response = await new Promise((resolve, reject) => {
+                const query = "UPDATE jobs SET name = ? WHERE id = ?";
+
+                connection.query(query, [name, id], (err, result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result.affectedRows);
+                })
+            });
+
+            return response === 1 ? true : false;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
 }
+
 
 module.exports = DbService;
